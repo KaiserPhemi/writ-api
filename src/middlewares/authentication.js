144021e2ba6a8a94fdf9eaabc725df5b8186e796
KaiserPhemi <oluwafemi.akinwa@andela.com>
2019-04-03 11:13:05 +0100

    ft(auth)
    - implement token verification
    
    [Starts #165078956]

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -43,15 +43,88 @@ const auth = {
     next();
   },
 
-  verifyToken() { },
-
   /**
-   *
-   * @param {*} req
-   * @param {*} res
-   * @param {*} next
+   * @desc verifies token
+   * @param {object} req
+   * @param {object} res
+   * @param {func} next
+   */
+  verifyToken(req, res, next) {
+    const token = req.headers['x-access-token']
+      || req.headers.authorization;
+    if (!token) {
+      return res
+        .status(400)
+        .send({
+          message: 'Unauthorized Access.'
+        });
+    }
+    jwt.verify(token, secretKey, (err, decode) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({
+            message: 'Invalid Token'
+          });
+      }
+      db.User
+        .findByPk(decode.userId)
+        .then((user) => {
+          if (!user) {
+            return res
+              .status(404)
+              .send({
+                message: 'Account does not exist. Kindly signup.'
+              });
+          }
+          if (!user.active) {
+            return res
+              .status(404)
+              .send({
+                message: 'Please sign in to access account.'
+              });
+          }
+          req.tokenDecode = decode;
+          req.tokenDecode.roleId = user.roleId;
+        })
+        .catch(err => {
+          return res
+            .status(500)
+            .send({
+              message: 'Error encountered',
+              err
+            });
+        });
+    });
+    next();
+  },
+
+   /**
+   * @desc validates if user has admin rights
+   * @param {object} req
+   * @param {object} res
+   * @param {func} next
    */
   checkAdminRights(req, res, next) {
+    db.Role
+      .findByPk(req.tokenDecode.roleId)
+      .then(role => {
+        if (role.title !== 'admin') {
+          return res
+            .status(403)
+            .send({
+              message: 'You are not permitted to perform this action.'
+            });
+        }
+      })
+      .catch(err => {
+        return res
+          .status(500)
+          .send({
+            message: 'Error encountered',
+            err
+          });
+      });
     next();
   },
 
