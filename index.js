f18f6e70cdd52828daf02223080980ee2c52b588
KaiserPhemi <oluwafemi.akinwa@andela.com>
2019-10-03 19:12:29 +0100

    ch(dependencies): remove database abstraction tool
    - remove sequelize
    
    [Starts #168925840]

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 // dependencies
-import * as dotenv from 'dotenv';
-import app from './src/server';
+import * as dotenv from "dotenv";
+import { Client } from "pg";
+import app from "./src/server";
 
 // port
 const port = process.env.PORT || 5555;
-app.set('port', port);
+app.set("port", port);
 
 // disable logging
 dotenv.config({ silent: true });
 
+// database connection
+const dbUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.PROD_DATABASE
+    : process.env.DEV_DATABASE;
+const client = new Client({ dbUrl });
+client.connect();
+
 // start app and listen on parsed port
 if (!module.parent) {
-  app.listen(port, (error) => {
+  app.listen(port, error => {
     if (error) {
       throw error;
     }
