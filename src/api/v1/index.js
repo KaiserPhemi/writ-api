2d528758a68a3c9c85a0215dad113edf88903b7c
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-05-08 14:37:58 +0100

    ch(backend)-
    -clean up
    
    - refactor middleware

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -4,14 +4,13 @@ import express from "express";
 // routers
 import roleRouter from "./role/roleRoutes";
 import userRouter from "./user/userRoutes";
-import authRouter from './auth/authRoutes'
+import authRouter from "./auth/authRoutes";
 
 const mainRouter = express.Router();
 
 // mount routes
 mainRouter.use("/roles", roleRouter);
 mainRouter.use("/users", userRouter);
-mainRouter.use('/auth', authRouter)
-
+mainRouter.use("/auth", authRouter);
 
 export default mainRouter;
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,53 +1,52 @@
 // third-party libraries
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 /**
- * @desc 
+ * @desc
  */
 const auth = {
   /**
    * @desc verifies the token supplied
-   * @param {object} req 
-   * @param {object} res 
-   * @param {object} next 
+   * @param {object} req
+   * @param {object} res
+   * @param {object} next
    */
-  async verifyToken(req, res, next) { 
-    const token = req.header('x-auth-token')
+  async verifyToken(req, res, next) {
+    const token = req.header("x-auth-token");
     if (!token) {
-      return res.status(401)
-        .send({
-          message: 'Unauthorized. Please login.'
-        })
+      return res.status(401).send({
+        message: "Unauthorized. Please login.",
+      });
     }
     try {
-      const decoded = jwt.verify(token, process.env.JWT_KEY)
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
       req.user = decoded;
       next();
     } catch (error) {
-      return res.status(400)
-        .send({
-          message: 'Invalid token',
-          error
-        })
+      return res.status(400).send({
+        message: "Invalid token",
+        error,
+      });
     }
   },
 
   /**
    * @desc check if user has admin rights
-   * @param {object} req 
-   * @param {object} res 
-   * @param {object} next 
+   * @param {object} req
+   * @param {object} res
+   * @param {object} next
    */
   async checkAdminRights(req, res, next) {
     const { user } = req;
-    if (user.roleTitle != 'super-admin' || user.roleTitle != 'team-admin') {
-      return res.status(403)
-        .send({
-          message: 'Access forbidden.'
-        })
+    if (user.roleTitle != "super-admin" || user.roleTitle != "team-admin") {
+      return res.status(403).send({
+        message: "Access forbidden.",
+      });
     }
     next();
-  } 
-}
+  },
+
+  async checkExistingUser(req, res, next) {},
+};
 
 export default auth;
